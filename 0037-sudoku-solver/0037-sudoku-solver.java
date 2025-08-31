52680ee6431a74e3b100fd86fd0633541843e78f
Ejjaganisirini <23951a04j8@iare.ac.in>
2025-08-31 23:07:26 +0530

    Time: 201 ms (69.02%), Space: 41.3 MB (43.93%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    // Try digits 1 to 9
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = '.'; // backtrack
+                            }
+                        }
+                    }
+                    return false; // no valid number found
+                }
+            }
+        }
+        return true; // solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        // Check row and column
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 subgrid
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
