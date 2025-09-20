492445733c58120608718c11d2f22c0b5aa4a842
Ejjaganisirini <23951a04j8@iare.ac.in>
2025-09-20 12:07:00 +0530

    Time: 0 ms (100%), Space: 43.3 MB (5.68%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+/*class Solution {
     public int maxDepth(TreeNode root) {
         if(root==null){
             return 0;
@@ -37,5 +37,18 @@ class Solution {
             depth++;
         }
         return depth;
+    }
+}*/
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return helper(root,0);
+    }
+    public int helper(TreeNode root,int curr){
+        if(root==null){
+            return curr;
+        }
+        int a=helper(root.left,curr+1);
+        int b=helper(root.right,curr+1);
+        return Math.max(a,b);
     }
 }
